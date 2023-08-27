ea44976d05fcd6fd276ab9f668405eeecbf89ba6
Rishita Srivastava <rixsri.13@gmail.com>
2023-08-28 00:53:57 +0530

    property page added

--- src/pages/PropertyDetails.js
+++ src/pages/PropertyDetails.js
@@ -1,71 +1,99 @@
-import React from 'react';
-import {housesData} from '../data'
-import { useParams } from 'react-router-dom';
+import React from "react";
+import { housesData } from "../data";
+import { useParams } from "react-router-dom";
 import { BiBed, BiBath, BiArea } from "react-icons/bi";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 const PropertyDetails = () => {
-  const {id} = useParams();
-  const house = housesData.find(house => {
+  const { id } = useParams();
+  const house = housesData.find((house) => {
     return house.id === parseInt(id);
-  })
+  });
   console.log(house);
-  return <section>
-    <div className='container mx-auto min-h-[800px] mb-14'>
-      <div className='flex flex-col lg:flex-row lg:items-center lg:justify-between'>
-        <div>
-          <h2 className='text-2xl font-semibold'>{house.name}</h2>
-          <h3 className='text-lg mb-4'>{house.address}</h3>
-        </div>
-        <div className='mb-4 lg:mb-0 flex gap-x-2 text-sm'>
-          <div className='bg-green-500 text-white px-3 py-1 rounded-full'>{house.type}</div>
-          <div className='bg-purple-500 text-white px-3 py-1 rounded-full'>{house.country}</div>
-        </div>
-        <div className='text-purple-700 font-semibold'>$ {house.price}</div>
-      </div> 
-      <div className='flex flex-col items-start gap-8 lg:flex-row'>
-        <div className='max-w-[768px]'>
-          <div className='mb-8'>
-            <img src={house.image} alt=""/>
+  return (
+    <section>
+      <div className="container mx-auto min-h-[800px] mb-14">
+        <div className="flex flex-col lg:flex-row lg:items-center lg:justify-between">
+          <div>
+            <h2 className="text-2xl font-semibold">{house.name}</h2>
+            <h3 className="text-lg text-gray-500 mb-4 mt-4 lg:mt-0">{house.address}</h3>
           </div>
-          <div className='flex gap-x-6 text-purple-700 mb-6'>
-            <div className='flex gap-x-2 items-center'>
-              <BiBed className='text-2xl'/>
-              <div>{house.bedrooms}</div>
+          <div className="mb-4 lg:mb-0 flex gap-x-2 text-sm">
+            <div className="bg-green-500 text-white px-3 py-1 rounded-full">
+              {house.type}
             </div>
-            <div className='flex gap-x-2 items-center'>
-              <BiBath className='text-2xl'/>
-              <div>{house.bathrooms}</div>
-            </div>
-            <div className='flex gap-x-2 items-center'>
-              <BiArea className='text-2xl'/>
-              <div>{house.surface}</div>
+            <div className="bg-purple-500 text-white px-3 py-1 rounded-full">
+              {house.country}
             </div>
           </div>
-          <div>{house.description}</div>
+          <div className="text-purple-700 font-semibold mb-4 lg:mb-0">$ {house.price}</div>
         </div>
-        <div className='flex-1 bg-gray-50 w-full mb-8 border border-gray-200 rounded-lg px-6 py-8'>
-          <div className='flex items-center gap-x-4 mb-8'>
-            <div className='w-20 h-20 p-1 border border-gray-300 rounded-full'>
-              <img src={house.agent.image} alt=""/>
+        <div className="flex flex-col items-start gap-8 lg:flex-row">
+          <div className="max-w-[768px]">
+            <div className="mb-8">
+              <img src={house.image} alt="" />
             </div>
-            <div className='font-bold text-lg'>{house.agent.name}</div>
-            <Link to='' className='text-purple-700 text-sm'>View Listing</Link>
+            <div className="flex gap-x-6 text-purple-700 mb-6">
+              <div className="flex gap-x-2 items-center">
+                <BiBed className="text-2xl" />
+                <div>{house.bedrooms}</div>
+              </div>
+              <div className="flex gap-x-2 items-center">
+                <BiBath className="text-2xl" />
+                <div>{house.bathrooms}</div>
+              </div>
+              <div className="flex gap-x-2 items-center">
+                <BiArea className="text-2xl" />
+                <div>{house.surface}</div>
+              </div>
+            </div>
+            <div>{house.description}</div>
           </div>
-          <form className='flex flex-col gap-y-4'>
-            <input className='border border-gray-200 focus:border-purple-700 outline-none rounded w-full px-4 h-14 text-sm' placeholder='Name*' type='text'/>
-            <input className='border border-gray-200 focus:border-purple-700 outline-none rounded w-full px-4 h-14 text-sm' placeholder='Email*'  type='text'/>
-            <input className='border border-gray-200 focus:border-purple-700 outline-none rounded w-full px-4 h-14 text-sm' placeholder='Phone*'  type='text'/>
-            <textarea className='border border-gray-200 focus:border-purple-700 outline-none resize-none rounded w-full p-4 h-36 text-sm text-gray-400' placeholder='Message*' defaultValue='Hello I am interested in [Property]' ></textarea>
-            <div className='flex gap-x-2'>
-              <button className='bg-purple-700 hover:bg-purple-800 text-white rounded p-4 text-sm w-full transition'>Send Message</button>
-              <button className='border border-purple-700 text-purple-700 hover:border-purple-500 hover:text-purple-500 p-4 w-full text-sm transition rounded'>Call</button>
+          <div className="flex-1 bg-gray-50 w-full mb-8 border border-gray-200 rounded-lg px-6 py-8">
+            <div className="flex items-center gap-x-4 mb-8">
+              <div className="w-20 h-20 p-1 border border-gray-300 rounded-full">
+                <img src={house.agent.image} alt="" />
+              </div>
+              <div className="font-bold text-lg">{house.agent.name}</div>
+              <Link to="" className="text-purple-700 text-sm">
+                View Listing
+              </Link>
             </div>
-          </form>
+            <form className="flex flex-col gap-y-4">
+              <input
+                className="border border-gray-200 focus:border-purple-700 outline-none rounded w-full px-4 h-14 text-sm"
+                placeholder="Name*"
+                type="text"
+              />
+              <input
+                className="border border-gray-200 focus:border-purple-700 outline-none rounded w-full px-4 h-14 text-sm"
+                placeholder="Email*"
+                type="text"
+              />
+              <input
+                className="border border-gray-200 focus:border-purple-700 outline-none rounded w-full px-4 h-14 text-sm"
+                placeholder="Phone*"
+                type="text"
+              />
+              <textarea
+                className="border border-gray-200 focus:border-purple-700 outline-none resize-none rounded w-full p-4 h-36 text-sm text-gray-400"
+                placeholder="Message*"
+                defaultValue="Hello I am interested in [Property]"
+              ></textarea>
+              <div className="flex gap-x-2">
+                <button className="bg-purple-700 hover:bg-purple-800 text-white rounded p-4 text-sm w-full transition">
+                  Send Message
+                </button>
+                <button className="border border-purple-700 text-purple-700 hover:border-purple-500 hover:text-purple-500 p-4 w-full text-sm transition rounded">
+                  Call
+                </button>
+              </div>
+            </form>
+          </div>
         </div>
       </div>
-    </div>
-  </section>
+    </section>
+  );
 };
 
 export default PropertyDetails;
